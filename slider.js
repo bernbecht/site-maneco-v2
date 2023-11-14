ddc7511544e60a2d825d2b5df59ae0d30e8b2d27
Bernardo Bechtold <bernardo.bechtold@shopify.com>
2023-11-14 22:01:31 +0100

    🚚  split scss into different files

---
+++ slider.js
@@ -0,0 +1,42 @@
+import "./slider.scss";
+
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide, index) => {
+  slide.style.transform = `translateX(${index * 100}%)`;
+});
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// select next slide button
+const nextSlide = document.querySelector(".btn-next");
+
+// add event listener and next slide functionality
+nextSlide.addEventListener("click", function () {
+  if (curSlide === maxSlide) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
+
+// select next slide button
+const prevSlide = document.querySelector(".btn-prev");
+
+// add event listener and next slide functionality
+prevSlide.addEventListener("click", function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide;
+  } else {
+    curSlide--;
+  }
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+});
